fe9f695fe82b773a042ecae1538d0dbb22396ce4
Stuart Halberg <shalberg@dynastyse.com>
2017-06-20 15:42:05 -0400

    first

---
+++ server.js
@@ -0,0 +1,176 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+var request = require("request");
+var cheerio = require("cheerio");
+mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
+
+// use morgan and bodyparser with our app
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// make public a static dir
+app.use(express.static('public'));
+
+var databaseUri = 'mongodb://localhost/newsscraper';
+
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI);
+} else {
+	mongoose.connect(databaseUri);
+}
+
+var db = mongoose.connection;
+
+// show any mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+
+// And we bring in our Note and Article models
+//var Note = require('./models/Note.js');
+//var Article = require('./models/Article.js');
+
+
+// Routes
+// ======
+
+// Simple index route
+app.get('/', function(req, res) {
+  res.send(index.html);
+});
+
+// A GET request to scrape the echojs website.
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.echojs.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+
+
+// this will get the articles we scraped from the mongoDB
+app.get('/articles', function(req, res){
+
+	// grab every doc in the Articles array
+	Article.find({}, function(err, doc){
+		// log any errors
+		if (err){
+			console.log(err);
+		} 
+		// or send the doc to the browser as a json object
+		else {
+			console.log("WORKING STUFF");
+			res.json(doc);
+		}
+	});
+});
+
+// grab an article by it's ObjectId
+app.get('/articles/:id', function(req, res){
+	// using the id passed in the id parameter, 
+	// prepare a query that finds the matching one in our db...
+	Article.findOne({'_id': req.params.id})
+	// and populate all of the notes associated with it.
+	.populate('note')
+	// now, execute our query
+	.exec(function(err, doc){
+		// log any errors
+		if (err){
+			console.log(err);
+		} 
+		// otherwise, send the doc to the browser as a json object
+		else {
+			res.json(doc);
+		}
+	});
+});
+
+
+// replace the existing note of an article with a new one
+// or if no note exists for an article, make the posted note it's note.
+app.post('/articles/:id', function(req, res){
+	// create a new note and pass the req.body to the entry.
+	var newNote = new Note(req.body);
+
+	// and save the new note the db
+	newNote.save(function(err, doc){
+		// log any errors
+		if(err){
+			console.log(err);
+		} 
+		// otherwise
+		else {
+			// using the Article id passed in the id parameter of our url, 
+			// prepare a query that finds the matching Article in our db
+			// and update it to make it's lone note the one we just saved
+			Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
+			// execute the above query
+			.exec(function(err, doc){
+				// log any errors
+				if (err){
+					console.log(err);
+				} else {
+					// or send the document to the browser
+					res.send(doc);
+				}
+			});
+		}
+	});
+});
+
+
+
+
+
+
+
+// listen on port 3000
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
+});
